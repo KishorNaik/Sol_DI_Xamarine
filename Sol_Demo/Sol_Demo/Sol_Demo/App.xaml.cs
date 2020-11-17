6bee766a0ddf7bf276dc15ed6a22588bccb5f7d0
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-17 21:00:39 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sol_Demo.Services;
+using Sol_Demo.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sol_Demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureService();
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        private static IServiceProvider ConfigureService()
+        {
+            var services = new ServiceCollection();
+
+            // Service
+            services.AddTransient<IUserService, UserService>();
+
+            // ViewModel
+            services.AddTransient<UserViewModel>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Sol_Demo/ViewModels/UserViewModel.cs
@@ -0,0 +1,83 @@
+﻿using MvvmHelpers;
+using Sol_Demo.Models;
+using Sol_Demo.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Sol_Demo.ViewModels
+{
+    public class UserViewModel : BaseViewModel
+    {
+        private readonly IUserService userService = null;
+
+        public UserViewModel(IUserService userService)
+        {
+            this.userService = userService;
+            UserM = new UserModel();
+
+            IsCancelEnabled = true;
+            IsSubmitEnabled = false;
+
+            DisplayCommand = new Command(() => this.OnDisplay());
+            CancelCommand = new Command<ContentPage>((contentPage) => this.OnCancel(contentPage));
+        }
+
+        private UserModel userM;
+
+        public UserModel UserM
+        {
+            get => userM;
+            set => base.SetProperty(ref userM, value);
+        }
+
+        private String display;
+
+        public String Display
+        {
+            get => display;
+            set => SetProperty(ref display, value);
+        }
+
+        private bool isCancelEnabled;
+
+        public bool IsCancelEnabled
+        {
+            get => isCancelEnabled;
+            set => base.SetProperty(ref isCancelEnabled, value);
+        }
+
+        private bool isSubmitEnabled;
+
+        public bool IsSubmitEnabled
+        {
+            get => isSubmitEnabled;
+            set => base.SetProperty(ref isSubmitEnabled, value);
+        }
+
+        public ICommand DisplayCommand { get; set; }
+
+        public ICommand CancelCommand { get; set; }
+
+        private void OnDisplay()
+        {
+            Display = userService.DisplayUsers(UserM);
+            IsCancelEnabled = false;
+            IsSubmitEnabled = true;
+        }
+
+        private void OnCancel(ContentPage contentPage)
+        {
+            Display = String.Empty;
+            UserM.FirstName = String.Empty;
+            UserM.LastName = String.Empty;
+            IsSubmitEnabled = false;
+            IsCancelEnabled = true;
+
+            Entry entryObj = contentPage.FindByName<Entry>("txtFirstName");
+            entryObj.Focus();
+        }
+    }
+}
